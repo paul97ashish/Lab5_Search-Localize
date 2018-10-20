30b0fe41f1bdc866374d4c6af4260f0d637de8aa
zakessad <zakaria.essadaoui@mail.mcgill.ca>
2018-10-20 16:04:26 -0400

    onec

--- src/ca/mcgill/ecse211/Lab5/RingSearch.java
+++ src/ca/mcgill/ecse211/Lab5/RingSearch.java
@@ -14,6 +14,8 @@ public class RingSearch extends Thread{
 	private static ObstacleDetect sensor;
 	private static double LastX, LastY;
 	private static Navigation navigation;
+	private static final int SPEED=50;
+	int ringValue=5;
 	public RingSearch (ObstacleDetect sens, Navigation navig) {
 		SensorMotor.rotate(90);
 		detect=new ColorDetection();
@@ -40,6 +42,14 @@ public class RingSearch extends Thread{
 				if(checkRing()) {
 					int distance=sensor.readUSDistance();
 					navigation.travelTo(LastX+distance-10, LastY);
+					leftMotor.setSpeed(SPEED);
+					rightMotor.setSpeed(SPEED);
+					ringValue=detect.detect();
+					while (ringValue==5) {
+						leftMotor.forward();
+						rightMotor.forward();
+					}
+					
 					
 				}
 				
@@ -54,6 +64,10 @@ public class RingSearch extends Thread{
 		int distance2=sensor.readUSDistance();
 		int distance3=sensor.readUSDistance();
 		return (distance1<100)&&(distance2<100)&&(distance3<100);
+	}
+	
+	public int ringValue() {
+		return ringValue+1;
 	}
 	
 }
