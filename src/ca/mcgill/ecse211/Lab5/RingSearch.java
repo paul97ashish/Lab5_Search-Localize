56ebef4f4efe1a885413c2b476eb5b2dd24b39b4
zakessad <zakaria.essadaoui@mail.mcgill.ca>
2018-10-20 15:54:51 -0400

    nmewk

---
+++ src/ca/mcgill/ecse211/Lab5/RingSearch.java
@@ -0,0 +1,59 @@
+package ca.mcgill.ecse211.Lab5;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+
+public class RingSearch extends Thread{
+	private static final EV3MediumRegulatedMotor SensorMotor=new EV3MediumRegulatedMotor(LocalEV3.get().getPort("D"));
+	private static EV3LargeRegulatedMotor leftMotor = Lab5.getLeftMotor();
+	private static EV3LargeRegulatedMotor rightMotor = Lab5.getRightMotor();
+	private static ColorDetection detect;
+	private static int distance;
+	private static int Last;
+	private static ObstacleDetect sensor;
+	private static double LastX, LastY;
+	private static Navigation navigation;
+	public RingSearch (ObstacleDetect sens, Navigation navig) {
+		SensorMotor.rotate(90);
+		detect=new ColorDetection();
+		sensor=sens;
+		navigation=navig;
+	}
+	
+	public void run() {
+		Last=sensor.readUSDistance();
+		while (Lab5.navigation.isNavigating()) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			distance=sensor.readUSDistance();
+			
+			if ((Last-distance)>50) {
+				leftMotor.stop(true);
+				rightMotor.stop();
+				LastX=Lab5.odometryDisplay.getXYT()[0];
+				LastY=Lab5.odometryDisplay.getXYT()[1];
+				if(checkRing()) {
+					int distance=sensor.readUSDistance();
+					navigation.travelTo(LastX+distance-10, LastY);
+					
+				}
+				
+			}
+			
+			
+		}
+		
+	}
+	public boolean checkRing() {
+		int distance1=sensor.readUSDistance();
+		int distance2=sensor.readUSDistance();
+		int distance3=sensor.readUSDistance();
+		return (distance1<100)&&(distance2<100)&&(distance3<100);
+	}
+	
+}
